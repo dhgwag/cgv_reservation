f3a36b499fdda6e9859f885ab57bbe5c15874a28
rhkrehdgns <rhkrehdgns@gmail.com>
2022-12-26 17:32:11 +0900

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ movie_type = 'IMAX'
 theater_area = '서울'
 theater = '용산아이파크몰'
 day = '7'
-movie_time = '14:40'
+movie_time = '26:10'
+adult = '2'
+youth = '0'
+row_from = 5
+row_to = 8
+column_from = 10
+column_to = 20
 
 telegram_token = ""
 telegram_id = ""
